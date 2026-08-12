e969dec397b99237481be7de7f4af9b72bf7a6e4
hamza <naqshband.hamza@gmail.com>
2026-08-12 23:41:48 +0500

    progress

---
+++ tests/ring_buffer/SpscRingBufferTest.cpp
@@ -0,0 +1,57 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "ring_buffer/SpscRingBuffer.h"
+
+TEST_CASE("Ring buffer starts empty")
+{
+    llt::SpscRingBuffer<int, 4> queue;
+
+    REQUIRE(queue.empty());
+    REQUIRE_FALSE(queue.full());
+    REQUIRE(queue.size() == 0);
+}
+
+TEST_CASE("Ring buffer pushes and pops")
+{
+    llt::SpscRingBuffer<int, 4> queue;
+
+    REQUIRE(queue.push(10));
+    REQUIRE(queue.push(20));
+
+    REQUIRE(queue.size() == 2);
+
+    auto first = queue.pop();
+
+    REQUIRE(first.has_value());
+    REQUIRE(first.value() == 10);
+
+    auto second = queue.pop();
+
+    REQUIRE(second.has_value());
+    REQUIRE(second.value() == 20);
+
+    REQUIRE(queue.empty());
+}
+
+TEST_CASE("Ring buffer detects full state")
+{
+    llt::SpscRingBuffer<int, 2> queue;
+
+    REQUIRE(queue.push(10));
+    REQUIRE(queue.push(20));
+
+    REQUIRE(queue.full());
+
+    REQUIRE_FALSE(queue.push(30));
+
+    REQUIRE(queue.size() == 2);
+}
+
+TEST_CASE("Ring buffer cannot pop from empty buffer")
+{
+    llt::SpscRingBuffer<int, 2> queue;
+
+    auto result = queue.pop();
+
+    REQUIRE_FALSE(result.has_value());
+}
